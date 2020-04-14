56875f2ce8edd75c75d19e9f16aa0eb1f910fead
Gianluca Maccioni <gianluca.maccioni.uk@gmail.com>
2020-04-14 20:11:00 +0100

    StreamEdit: fetch data locally

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-function StreamEdit(props) {
-  console.log('props', props);
-  return (
-    <div>
-      StreamEdit
-    </div>
-  )
+class StreamEdit extends React.Component {
+
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+
+  render() {
+    console.log('this.props', this.props);
+    return (
+      <div>
+        StreamEdit
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] }
 }
 
-export default connect(mapStateToProps, null)(StreamEdit);
+export default connect(
+  mapStateToProps,
+  { fetchStream }
+)(StreamEdit);
