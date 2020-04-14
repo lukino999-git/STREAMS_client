8320f6b718b31337676858503f9f676b13f0b6cd
Gianluca Maccioni <gianluca.maccioni.uk@gmail.com>
2020-04-14 17:18:46 +0100

    StreamList: render admin buttons if logged in

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -8,10 +8,24 @@ class StreamList extends React.Component {
     this.props.fetchStreams();
   }
 
+  // render delete and update buttons
+  renderAdmin(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">EDIT</button>
+          <button className="ui button negative">DELETE</button>
+        </div>
+      )
+    }
+  }
+
+  // render list of streams
   renderList() {
     return this.props.streams.map(stream => {
       return (
         <div className="item" key={stream.id}>
+          {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
             {stream.title}
@@ -24,8 +38,8 @@ class StreamList extends React.Component {
     })
   }
 
+  // class render method
   render() {
-
     return (
       <div>
         <h2>Streams</h2>
@@ -36,7 +50,10 @@ class StreamList extends React.Component {
 }
 
 const mapStateToProps = state => {
-  return { streams: Object.values(state.streams) }
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId
+  }
 }
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
