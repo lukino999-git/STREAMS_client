3a555376af80e1497a67478361600ab4db3e088b
Gianluca Maccioni <gianluca.maccioni.uk@gmail.com>
2020-04-14 17:40:34 +0100

    StreamList: add create button if logged in

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { connect } from 'react-redux';
 import { fetchStreams } from '../../actions';
+import { Link } from 'react-router-dom';
 
 class StreamList extends React.Component {
 
   componentDidMount() {
     this.props.fetchStreams();
   }
+
 
   // render delete and update buttons
   renderAdmin(stream) {
@@ -19,6 +21,7 @@ class StreamList extends React.Component {
       )
     }
   }
+
 
   // render list of streams
   renderList() {
@@ -38,21 +41,40 @@ class StreamList extends React.Component {
     })
   }
 
+
+  // render create button
+  renderCreate() {
+    if (this.props.isSignedIn) {
+      return (
+        <div style={{ textAlign: "right" }}>
+          <Link to="/streams/new" className="ui button primary">
+            Create stream
+          </Link>
+        </div>
+      )
+    }
+  }
+
+
   // class render method
   render() {
     return (
       <div>
         <h2>Streams</h2>
         <div className="ui celled list">{this.renderList()}</div>
+        {this.renderCreate()}
       </div>
     )
   }
 }
 
+
+// map state to Props
 const mapStateToProps = state => {
   return {
     streams: Object.values(state.streams),
-    currentUserId: state.auth.userId
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn
   }
 }
 
